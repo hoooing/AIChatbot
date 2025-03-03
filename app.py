11c7c3a47f6ef4fe64aedfc304d938517e4940df
Donghyeon Kim <kdh010125@gmail.com>
2025-03-03 17:37:08 -0500

    updatE

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from chatbot import generate_response  # Import AI logic
 import re
 import os
 
-app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
+app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
