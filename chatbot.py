422e3ad8f3510fdade0f2307df617aec3ff1a8a5
Donghyeon Kim <kdh010125@gmail.com>
2025-03-03 19:23:01 -0500

    updatE

--- chatbot.py
+++ chatbot.py
@@ -47,6 +47,6 @@ def generate_response(user_input):
 
     # Append AI response to history
     chat_history.append(types.Content(role="model", parts=[types.Part.from_text(text=ai_response)]))
-    if user_input in "순국":
+    if "순국" in user_input:
         ai_response = "이 바텀게임 ㅈ도못하는 개버러지 개쓰레기같은 개비응신 마스터300점피크라고 구라치는 신짜오 정글 원툴 쓰레기새끼."
     return ai_response
